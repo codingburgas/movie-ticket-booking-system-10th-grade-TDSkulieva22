331b7c127c3ab56c056b8dc8c11c7869cef27cb7
TDSkulieva <TDSkulieva22@codingburgas.bg>
2025-06-13 17:46:21 +0300

    Create function for reserveTicket.

--- movie-tickets/Cinema/core/src/Booking.cpp
+++ movie-tickets/Cinema/core/src/Booking.cpp
@@ -51,3 +51,66 @@ void printSeatMap(const vector<Seat>& seats) {
         wcout << endl;
     }
 }
+
+void reserveTicket(int userId) {
+    DatabaseManager db;
+    ReservationInput input;
+
+    wcout << L"Enter town: "; getline(wcin, input.city);
+    wcout << L"Enter location: "; getline(wcin, input.location);
+    wcout << L"Enter date (YYYY-MM-DD): "; getline(wcin, input.date);
+
+    wstring programTable = input.city + L"Program";
+
+    wstring query = L"SELECT DISTINCT m.Title "
+        L"FROM Movies m "
+        L"JOIN [" + programTable + L"] p ON m.MovieId = p.MovieId "
+        L"WHERE p.Location = N'" + input.location + L"' AND p.Date = N'" + input.date + L"'";
+
+    vector<wstring> movies = db.getMovieTitles(query);
+
+    for (size_t i = 0; i < movies.size(); i++) {
+        wcout << i + 1 << L". " << movies[i] << endl;
+    }
+    int movieIndex;
+    wcout << L"Choose a movie: ";
+    wcin >> movieIndex; wcin.ignore();
+    input.movieTitle = movies[movieIndex - 1];
+
+    int programId = db.getProgramId(programTable, input.movieTitle, input.location, input.date);
+
+    vector<Seat> currentSeats = db.getSeatMap(programId);
+    printSeatMap(currentSeats);
+
+    int numTickets;
+    wcout << L"Number of tickets: "; wcin >> numTickets;
+
+    vector<Seat> selectedSeats;
+    for (int i = 0; i < numTickets; i++) {
+        int row, num;
+        wcout << L"Seat #" << i + 1 << L" - Row: "; wcin >> row;
+        wcout << L"Seat #" << i + 1 << L" - Number: "; wcin >> num;
+
+        bool isFree = true;
+        for (const auto& s : currentSeats)
+            if (s.row == row && s.number == num && s.isReserved)
+                isFree = false;
+
+        if (isFree) {
+            selectedSeats.push_back({ row, num, false });
+        }
+        else {
+            wcout << L"This seat has already been booked. Choose another." << endl;
+            i--;
+        }
+    }
+
+    if (db.reserveSeats(programId, selectedSeats, userId)) {
+        system("cls");
+        wcout << L"\n Successful reservation!\n\n";
+        printSeatMap(db.getSeatMap(programId));
+    }
+    else {
+        wcout << L" Error.\n";
+    }
+}
